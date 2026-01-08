68de71144cc25e6fa00e45003f5b700759992a79
David Erick <googserick@gmail.com>
2026-01-07 23:23:36 -0300

    feat: teste de web scraping temperatura Recife

---
+++ main.py
@@ -0,0 +1,17 @@
+import requests;
+from bs4 import BeautifulSoup;
+
+link = "https://www.climatempo.com.br/previsao-do-tempo/15-dias/cidade/259/recife-pe"
+
+requisicao = requests.get(link);
+print(requisicao);  
+
+site = BeautifulSoup(requisicao.text, "html.parser"); 
+
+temperatura = site.find("span", class_="_margin-r-15");
+print("------------------------------------------")
+print("|                                        |")
+print(f"|      A temperatura em Recife é: {temperatura.get_text()}C   |")
+print("|            by: ClimaTempo              |")
+print("|                                        |")
+print("------------------------------------------")
